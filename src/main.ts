51e806fc6ce9ef57fa6de1c914a7cc667e05c7e8
Pedro Matsubara <pedro.matsubara.pereira@gmail.com>
2024-12-03 10:00:42 -0300

    feat(cors): Enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { GlobalExceptionFilter } from "./common/filters/global-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  });
 
   app.useGlobalFilters(new GlobalExceptionFilter());
 
